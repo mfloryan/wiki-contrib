dfce00650412b0683097809d109e5747db67e810
Marcin Floryan <marcin.floryan@gmail.com>
2025-01-13 18:21:10 +0100

    Colours into a separate file

---
+++ plot/Sweden Migration Statistics/colours.py
@@ -0,0 +1,13 @@
+from enum import EnumDict
+
+
+# Bang Wong's colorblind-friendly palette
+class BangWongColors(EnumDict):
+    BLACK = "#000000"
+    ORANGE = "#E69F00"
+    LIGHT_BLUE = "#56B4E9"
+    GREEN = "#009E73"
+    YELLOW = "#F0E442"
+    BLUE = "#0072B2"
+    RED_ORANGE = "#D55E00"
+    PINK = "#CC79A7"
--- plot/Sweden Migration Statistics/sweden_migation_by_country.py
+++ plot/Sweden Migration Statistics/sweden_migation_by_country.py
@@ -1,6 +1,5 @@
 import math
 from datetime import timedelta
-from enum import EnumDict
 
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
@@ -10,17 +9,7 @@ import requests_cache
 from dateutil import parser
 from matplotlib.patches import Patch
 
-
-# Bang Wong's colorblind-friendly palette
-class BangWongColors(EnumDict):
-    BLACK = "#000000"
-    ORANGE = "#E69F00"
-    LIGHT_BLUE = "#56B4E9"
-    GREEN = "#009E73"
-    YELLOW = "#F0E442"
-    BLUE = "#0072B2"
-    RED_ORANGE = "#D55E00"
-    PINK = "#CC79A7"
+from colours import BangWongColors
 
 
 def transform_data(response_data, metadata):
